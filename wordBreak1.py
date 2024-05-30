1950a9380f373ceeb5dcbed87421ed11164324f7
Gabriel <gmyuseff@uc.cl>
2024-05-30 09:50:06 -0400

    Solved wordBreak1

--- wordBreak1.py
+++ wordBreak1.py
@@ -1,43 +1,36 @@
 # Link: https://leetcode.com/problems/word-break
-# Status: Solution not accepted
+# Status: Accepted and beats 93.42%!
 
 # Problem statement: For a string "s" and a list of strings "wordDict"; 
 # return true if there is a possible way to form s, concatenating the strings in wordDict
 
 # Had tried with recursion; it works but in certain testcases it exceeds runtime
-# Current solution tries a Dynamic programming approach, however it is wrong.
-# It uses a pointer to a current index to start, checks if I can add any word that goes through that index and moves
-# It is a wrong algorithm, because it allows for the superposition of words
+# Solution uses a Dynamic programming approach; as the whole string must be created with the wordDict:
+# 1 -> Create a list of len(s) + 1 booleans with default false and list[0] = True; the list represents that I can create the substring up to each position. So if list[i] == True, it means I can use a combination of the strings in wordDict to create substring s[:i]
+# 2 -> Iterate through the string; if list[i] is true, then iterate thorugh each word w of wordDict, if s[i:i+len(w)] == w (the current substring of s starts with w) -> list[i + lw] == True
+# 3 -> When finishing the iteration, if the last boolean of the list is True, then we can recreate the whole string s with a concatenation of wordDict
 
 class Solution:
+    
     def wordBreak(self, s: str, wordDict: list[str]) -> bool:
-
-        index = 0
-        ls = len(s)
-        while index < ls:
-            for w in wordDict:
-                j = 0
-                lw = len(w)
-                found = False
-                while j < lw:
-                    try:
-                        if s[index-j:index-j+lw] == w:
-                            found = True
-                            break
-                        else:
-                            j += 1
-                    except:
-                        j = lw
-                        break
-                if j < lw:
-                    index += (lw - j)
-                    break
-            if not found:
-                return False
         
-        if index >= ls:
-            return True
+        ls = len(s)
 
+        if ls == 0:
+            return False
+        
+        solutions = [False for x in range(ls+1)]
+        solutions[0] = True
+
+        for i in range(ls):
+            if solutions[i]:
+                for w in wordDict:
+                    lw = len(w)
+                    if i + lw < ls + 1:
+                        if s[i:i+lw] == w:
+                            solutions[i+lw] = True
+        
+        return solutions[-1]
     
 
 if __name__ == "__main__":
@@ -50,4 +43,5 @@ if __name__ == "__main__":
     testcases : list[tuple[str,list[str]]] = [testcase_1, testcase_2, testcase_3, testcase_4]
 
     for ts in testcases:
+        print(ts)
         print(Solution.wordBreak(self=None, s=ts[0], wordDict=ts[1]))
